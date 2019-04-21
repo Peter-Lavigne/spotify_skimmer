f21b823108872d829f4e349f7659c38b05b7b621
Peter Lavigne <peter.lavigne@appfolio.com>
2019-04-21 13:54:06 -0700

    add lightning design system

--- src/index.js
+++ src/index.js
@@ -3,9 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { IconSettings } from '@salesforce/design-system-react';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
+ReactDOM.render(
+  <IconSettings iconPath="/icons">
+    <App />
+  </IconSettings>,
+  document.getElementById('root')
+);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
